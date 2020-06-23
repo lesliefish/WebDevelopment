f55121f69f8138ce97bbf929643d0868b95cdaf8
leiyu <yuleics@outlook.com>
2020-06-23 20:58:10 +0800

    update

---
+++ Demos/AngularJS1.2.9/phonecat/app/js/app.js
@@ -0,0 +1,24 @@
+
+var phonecatApp = angular.module('phonecatApp', [
+    'ngRoute',
+    'phonecatControllers'
+]);
+
+/**
+ *  路由
+ */
+phonecatApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/phones', {
+                templateUrl: 'partials/phoneList.asp',
+                controller: 'PhoneListCtrl'
+            }).
+            when('/phones/:phoneId', {
+                templateUrl: 'partials/phoneDetail.asp',
+                controller: 'PhoneDetailCtrl'
+            }).
+            otherwise({
+                redirectTo: '/phones'
+            });
+    }]);
--- Demos/AngularJS1.2.9/phonecat/app/js/controllers.js
+++ Demos/AngularJS1.2.9/phonecat/app/js/controllers.js
@@ -1,9 +1,26 @@
-var phonecatApp = angular.module('phonecatApp', []);
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
-    $http.get('phones/phones.json').success(function (data) {
-        $scope.phones = data;
-    });
+/**
+ *  列表页
+ */
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        $http.get('phones/phones.json').success(function (data) {
+            $scope.phones = data;
+        });
 
-    $scope.orderProp = 'age';
-});
+        $scope.orderProp = 'age';
+    }]);
+
+/**
+ * 详情页
+ */
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+    function ($scope, $routeParams, $http) {
+        $http.get('phones/' + $routeParams.phoneId + ".json")
+            .success(function (data) {
+                console.log(data);
+                $scope.phone = data;
+            })
+        $scope.phoneId = $routeParams.phoneId;
+    }]);
