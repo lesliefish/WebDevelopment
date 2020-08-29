f8355a196c184b0cf2af2f4c0046b774a8b08a87
leiyu <ylesliefish@gmail.com>
2020-08-30 00:04:39 +0800

    update

--- Vue/Vue零基础到实战/mainproject/news-app/src/App.vue
+++ Vue/Vue零基础到实战/mainproject/news-app/src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="app">
-  AAAA
+  <Banner />
   </div>
 </template>
 
 <script>
+import Banner from "./components/Banner"
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Banner,
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
