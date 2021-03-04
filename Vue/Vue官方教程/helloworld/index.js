72e8ab98a064f41ba48a128e3581df720ebbdb4c
leiyu <ylesliefish@gmail.com>
2021-03-04 23:12:00 +0800

    update

--- Vue/Vue官方教程/helloworld/index.js
+++ Vue/Vue官方教程/helloworld/index.js
@@ -18,7 +18,8 @@ var app = new Vue({
             { id: 0, text: '蔬菜' },
             { id: 1, text: '奶酪' },
             { id: 2, text: '随便其它什么人吃的东西' }
-        ]
+        ],
+        bindId:"myDiv"
     },
     methods: {
         onClick: function () {
